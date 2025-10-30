286dcde64b7eaec19551e1440a94c7b2d8c9d8e3
ocavue <ocavue@gmail.com>
2025-10-30 17:46:19 +1100

    feat!: support vitest v4

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { page, userEvent } from '@vitest/browser/context';
+import { page, userEvent } from 'vitest/browser';
 import { render } from 'vitest-browser-preact';
 import { useState } from 'preact/hooks';
 
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import { preact } from '@preact/preset-vite';
+import { playwright } from '@vitest/browser-playwright';
 
 export default defineConfig({
 	plugins: [preact()],
 	test: {
-		name: 'preact',
 		browser: {
 			enabled: true,
-			headless: true,
-			instances: [{ browser: 'chromium' }],
-			provider: 'playwright'
+			provider: playwright(),
+			instances: [{ browser: 'chromium' }]
 		}
 	}
 });
